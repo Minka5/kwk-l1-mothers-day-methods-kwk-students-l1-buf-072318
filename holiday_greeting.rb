1d1c3bfaf94f7737bc27634f472ef119bbc02352
Minka5 <madrakowsky5@gmail.com>
2018-07-24 15:46:08 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,5 +1,5 @@
 def holiday_greeting(reciever="mom",sender="your favorite child", holiday= "mother's day")
-  "Happy #{holiday},#{reci from #{sender}!"
+  "Happy #{holiday},#{reciver} from #{sender}!"
 end
 
 # puts holiday_greeting
